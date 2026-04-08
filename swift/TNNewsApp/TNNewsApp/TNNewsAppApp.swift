72d5cb47e3a1d2396818ce71648e58a7d5bf629b
= <=>
2026-04-08 12:04:36 +0200

    add swift app

---
+++ swift/TNNewsApp/TNNewsApp/TNNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TNNewsAppApp.swift
+//  TNNewsApp
+//
+//  Created by Hatem Bellaaj on 03/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct TNNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
